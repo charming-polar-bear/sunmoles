8785a348eca823da999dfdf4c6e71d1193506ca3
homer <oceanuszeng@gmail.com>
2017-10-20 09:56:08 +0800

    网站升级, 提供空白页面

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  get 'join-us' => 'oldweb#join-us'
-  get 'declaration' => 'oldweb#declaration'
-
-  get "/query" => "security#index"
-  get "/security" => "security#result"
+  # get 'join-us' => 'oldweb#join-us'
+  # get 'declaration' => 'oldweb#declaration'
+  #
+  # get "/query" => "security#index"
+  # get "/security" => "security#result"
   get '/fangwei' => "security#fangwei"
 
   # get '/test' => "security#test"
 
-  root 'landing_page#index'
+  # root 'landing_page#index'
+
+  ###升级用空白页面
+  root 'landing_page#empty'
 end
